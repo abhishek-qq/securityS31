88771dfbbcf0fb07a26de4c4a94f17a703e17420
Abhishek Mishra <prabhishekgeetamishra@gmail.com>
2021-09-21 07:05:54 +0530

    added enviormental variable

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//jshint esversion:6
-//jshint esversion:6
+
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -18,8 +18,8 @@ const userSchema= new mongoose.Schema({
 	email : String,
 	password:  String
 });
-const secret = "thisisalittlesecret";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = mongoose.model("User",userSchema);
